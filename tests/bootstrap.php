c8ee5fe47dd6e4775462c6f22d2f9e5faa797fc0
Damir Garifullin <dgarifullin@kirkazan.ru>
2015-01-24 15:45:48 +0300

    Switched to apigen/token-reflection

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,11 +9,15 @@
  */
 ini_set('display_errors', 1);
 
+define('AUTOLOAD_PATH', 'vendor/autoload.php');
+
 if (defined("AUTOLOAD_PATH")) {
-    if (is_file(__DIR__ . '/../' .AUTOLOAD_PATH)) {
-        $loader = include_once __DIR__ . '/../' . AUTOLOAD_PATH;
+    if (is_file(__DIR__ . '/../' . AUTOLOAD_PATH)) {
+        $loader = require __DIR__ . '/../' . AUTOLOAD_PATH;
         $loader->add('Go', __DIR__);
     } else {
-        throw new InvalidArgumentException("Cannot load custom autoload file located at ".AUTOLOAD_PATH);
+        throw new InvalidArgumentException(
+            "Cannot load custom autoload file located at " . __DIR__ . '/../' . AUTOLOAD_PATH
+        );
     }
 }
